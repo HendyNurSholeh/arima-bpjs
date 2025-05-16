4588f938db4c3eb312a71cb73d5a137fb8819377
Hendy Nur Sholeh <hendynursholeh7213@gmail.com>
2025-05-16 10:06:53 +0800

    feat: implemnet admin layout and dashboard

--- app/Views/admin/dashboard2.php
+++ app/Views/admin/dashboard2.php
@@ -7,23 +7,23 @@
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <title>Naive Bayes | Hipertensi </title>
     <!-- plugins:css -->
-    <link rel="stylesheet" href="/assets2/vendors/feather/feather.css">
-    <link rel="stylesheet" href="/assets2/vendors/mdi/css/materialdesignicons.min.css">
-    <link rel="stylesheet" href="/assets2/vendors/ti-icons/css/themify-icons.css">
-    <link rel="stylesheet" href="/assets2/vendors/font-awesome/css/font-awesome.min.css">
-    <link rel="stylesheet" href="/assets2/vendors/typicons/typicons.css">
-    <link rel="stylesheet" href="/assets2/vendors/simple-line-icons/css/simple-line-icons.css">
-    <link rel="stylesheet" href="/assets2/vendors/css/vendor.bundle.base.css">
-    <link rel="stylesheet" href="/assets2/vendors/bootstrap-datepicker/bootstrap-datepicker.min.css">
+    <link rel="stylesheet" href="/assets/vendors/feather/feather.css">
+    <link rel="stylesheet" href="/assets/vendors/mdi/css/materialdesignicons.min.css">
+    <link rel="stylesheet" href="/assets/vendors/ti-icons/css/themify-icons.css">
+    <link rel="stylesheet" href="/assets/vendors/font-awesome/css/font-awesome.min.css">
+    <link rel="stylesheet" href="/assets/vendors/typicons/typicons.css">
+    <link rel="stylesheet" href="/assets/vendors/simple-line-icons/css/simple-line-icons.css">
+    <link rel="stylesheet" href="/assets/vendors/css/vendor.bundle.base.css">
+    <link rel="stylesheet" href="/assets/vendors/bootstrap-datepicker/bootstrap-datepicker.min.css">
     <!-- endinject -->
     <!-- Plugin css for this page -->
-    <link rel="stylesheet" href="/assets2/vendors/datatables.net-bs4/dataTables.bootstrap4.css">
-    <link rel="stylesheet" type="text/css" href="/assets2/js/select.dataTables.min.css">
+    <link rel="stylesheet" href="/assets/vendors/datatables.net-bs4/dataTables.bootstrap4.css">
+    <link rel="stylesheet" type="text/css" href="/assets/js/select.dataTables.min.css">
     <!-- End plugin css for this page -->
     <!-- inject:css -->
-    <link rel="stylesheet" href="/assets2/css/style.css">
+    <link rel="stylesheet" href="/assets/css/style.css">
     <!-- endinject -->
-    <link rel="shortcut icon" href="/assets2/images/logo-circle.png" />
+    <link rel="shortcut icon" href="/assets/images/logo-circle.png" />
 </head>
 
 <body class="with-welcome-text">
@@ -40,12 +40,12 @@
                 </div>
                 <div>
                     <a class="navbar-brand brand-logo fw-bold" style="font-size: 0.9em;" href="index.html">
-                        <!-- <img src="/assets2/images/logo.svg" alt="logo" /> -->
+                        <!-- <img src="/assets/images/logo.svg" alt="logo" /> -->
                         Hipertensi | <sup>Naive Bayes</sup>
                     </a>
                     <a class="navbar-brand brand-logo-mini" href="index.html">
                         <sup> Naive Bayes</sup>
-                        <!-- <img src="/assets2/images/logo-mini.svg" alt="logo" /> -->
+                        <!-- <img src="/assets/images/logo-mini.svg" alt="logo" /> -->
                     </a>
                 </div>
             </div>
@@ -59,11 +59,11 @@
                 <ul class="navbar-nav ms-auto">
                     <li class="nav-item dropdown d-none d-lg-block user-dropdown">
                         <a class="nav-link" id="UserDropdown" href="#" data-bs-toggle="dropdown" aria-expanded="false">
-                            <img class="img-xs rounded-circle" src="/assets2/images/faces/face8.jpg" alt="Profile image">
+                            <img class="img-xs rounded-circle" src="/assets/images/faces/face8.jpg" alt="Profile image">
                         </a>
                         <div class="dropdown-menu dropdown-menu-right navbar-dropdown" aria-labelledby="UserDropdown">
                             <div class="dropdown-header text-center">
-                                <img class="img-md rounded-circle" src="/assets2/images/faces/face8.jpg"
+                                <img class="img-md rounded-circle" src="/assets/images/faces/face8.jpg"
                                     alt="Profile image">
                                 <p class="mb-1 mt-3 fw-semibold">Admin</p>
                             </div>
@@ -217,24 +217,24 @@
     </div>
     <!-- container-scroller -->
     <!-- plugins:js -->
-    <script src="/assets2/vendors/js/vendor.bundle.base.js"></script>
-    <script src="/assets2/vendors/bootstrap-datepicker/bootstrap-datepicker.min.js"></script>
+    <script src="/assets/vendors/js/vendor.bundle.base.js"></script>
+    <script src="/assets/vendors/bootstrap-datepicker/bootstrap-datepicker.min.js"></script>
     <!-- endinject -->
     <!-- Plugin js for this page -->
-    <script src="/assets2/vendors/chart.js/chart.umd.js"></script>
-    <script src="/assets2/vendors/progressbar.js/progressbar.min.js"></script>
+    <script src="/assets/vendors/chart.js/chart.umd.js"></script>
+    <script src="/assets/vendors/progressbar.js/progressbar.min.js"></script>
     <!-- End plugin js for this page -->
     <!-- inject:js -->
-    <script src="/assets2/js/off-canvas.js"></script>
-    <script src="/assets2/js/template.js"></script>
-    <script src="/assets2/js/settings.js"></script>
-    <script src="/assets2/js/hoverable-collapse.js"></script>
-    <script src="/assets2/js/todolist.js"></script>
+    <script src="/assets/js/off-canvas.js"></script>
+    <script src="/assets/js/template.js"></script>
+    <script src="/assets/js/settings.js"></script>
+    <script src="/assets/js/hoverable-collapse.js"></script>
+    <script src="/assets/js/todolist.js"></script>
     <!-- endinject -->
     <!-- Custom js for this page-->
-    <script src="/assets2/js/jquery.cookie.js" type="text/javascript"></script>
-    <script src="/assets2/js/dashboard.js"></script>
-    <!-- <script src="/assets2/js/Chart.roundedBarCharts.js"></script> -->
+    <script src="/assets/js/jquery.cookie.js" type="text/javascript"></script>
+    <script src="/assets/js/dashboard.js"></script>
+    <!-- <script src="/assets/js/Chart.roundedBarCharts.js"></script> -->
     <!-- End custom js for this page-->
 </body>
 
---
+++ app/Views/layout/admin_layout.php
@@ -0,0 +1,107 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="">
+    <meta name="author" content="">
+
+    <title>SB Admin 2 - Dashboard</title>
+
+    <!-- Custom fonts for this template-->
+    <link href="/assets2/vendor/fontawesome-free/css/all.min.css" rel="stylesheet" type="text/css">
+    <link
+        href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i"
+        rel="stylesheet">
+
+    <!-- Custom styles for this template-->
+    <link href="/assets2/css/sb-admin-2.min.css" rel="stylesheet">
+
+</head>
+
+<body id="page-top">
+
+    <!-- Page Wrapper -->
+    <div id="wrapper">
+
+        <!-- Sidebar -->
+        <ul class="navbar-nav bg-gradient-primary sidebar sidebar-dark accordion" id="accordionSidebar">
+
+            <!-- Sidebar - Brand -->
+            <a class="sidebar-brand d-flex align-items-center justify-content-center" href="index.html">
+                <div class="sidebar-brand-icon rotate-n-15">
+                    <i class="fas fa-laugh-wink"></i>
+                </div>
+                <div class="sidebar-brand-text mx-3">SB Admin <sup>2</sup></div>
+            </a>
+
+            <!-- Divider -->
+            <hr class="sidebar-divider my-0">
+
+            <!-- Nav Item - Dashboard -->
+            <li class="nav-item active">
+                <a class="nav-link" href="index.html">
+                    <i class="fas fa-fw fa-tachometer-alt"></i>
+                    <span>Dashboard</span></a>
+            </li>
+
+            <!-- Divider -->
+            <hr class="sidebar-divider">
+
+            <!-- Heading -->
+            <div class="sidebar-heading">
+                Addons
+            </div>
+            <!-- Nav Item - Charts -->
+            <li class="nav-item">
+                <a class="nav-link" href="charts.html">
+                    <i class="fas fa-fw fa-chart-area"></i>
+                    <span>Charts</span></a>
+            </li>
+
+            <!-- Nav Item - Tables -->
+            <li class="nav-item">
+                <a class="nav-link" href="tables.html">
+                    <i class="fas fa-fw fa-table"></i>
+                    <span>Tables</span></a>
+            </li>
+
+            <!-- Divider -->
+            <hr class="sidebar-divider d-none d-md-block">
+
+
+        </ul>
+        <!-- End of Sidebar -->
+
+        <?= self::renderSection('content'); ?>
+
+    </div>
+    <!-- End of Page Wrapper -->
+
+    <!-- Scroll to Top Button-->
+    <a class="scroll-to-top rounded" href="#page-top">
+        <i class="fas fa-angle-up"></i>
+    </a>
+    <!-- Bootstrap core JavaScript-->
+    <script src="/assets2/vendor/jquery/jquery.min.js"></script>
+    <script src="/assets2/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+
+    <!-- Core plugin JavaScript-->
+    <script src="/assets2/vendor/jquery-easing/jquery.easing.min.js"></script>
+
+    <!-- Custom scripts for all pages-->
+    <script src="js/sb-admin-2.min.js"></script>
+
+    <!-- Page level plugins -->
+    <script src="/assets2/vendor/chart.js/Chart.min.js"></script>
+
+    <!-- Page level custom scripts -->
+    <script src="js/demo/chart-area-demo.js"></script>
+    <script src="js/demo/chart-pie-demo.js"></script>
+
+</body>
+
+</html>
