04ad5ca09e1094bf3f86436345af92edd7d8f7a6
Hendy Nur Sholeh <hendynursholeh7213@gmail.com>
2025-05-17 11:53:03 +0800

    main yml

--- app/Views/layout/admin_layout.php
+++ app/Views/layout/admin_layout.php
@@ -12,7 +12,7 @@
     <title>ARIMA - BPJS</title>
 
     <!-- Custom fonts for this template-->
-    <link href="/assets2/vendor/fontawesome-free/css/all.min.css" rel="stylesheet" type="text/css">
+    <link href="/assets2/vendor2/fontawesome-free/css/all.min.css" rel="stylesheet" type="text/css">
     <link
         href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i"
         rel="stylesheet">
@@ -21,7 +21,7 @@
     <link href="/assets2/css/sb-admin-2.min.css" rel="stylesheet">
     <link rel="icon" type="image/png" href="/assets/images/bpjs-icon.png">
     
-    <link href="/assets2/vendor/datatables/dataTables.bootstrap4.min.css" rel="stylesheet">
+    <link href="/assets2/vendor2/datatables/dataTables.bootstrap4.min.css" rel="stylesheet">
 
 </head>
 
@@ -198,25 +198,25 @@
         <i class="fas fa-angle-up"></i>
     </a>
     <!-- Bootstrap core JavaScript-->
-    <script src="/assets2/vendor/jquery/jquery.min.js"></script>
-    <script src="/assets2/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+    <script src="/assets2/vendor2/jquery/jquery.min.js"></script>
+    <script src="/assets2/vendor2/bootstrap/js/bootstrap.bundle.min.js"></script>
 
     <!-- Core plugin JavaScript-->
-    <script src="/assets2/vendor/jquery-easing/jquery.easing.min.js"></script>
+    <script src="/assets2/vendor2/jquery-easing/jquery.easing.min.js"></script>
 
     <!-- Custom scripts for all pages-->
     <script src="/assets2/js/sb-admin-2.min.js"></script>
 
     <!-- Page level plugins -->
-    <script src="/assets2/vendor/chart.js/Chart.min.js"></script>
+    <script src="/assets2/vendor2/chart.js/Chart.min.js"></script>
 
     <!-- Page level custom scripts -->
     <script src="/assets2/js/demo/chart-area-demo.js"></script>
     <script src="/assets2/js/demo/chart-pie-demo.js"></script>
 
     <!-- Page level plugins -->
-    <script src="/assets2/vendor/datatables/jquery.dataTables.min.js"></script>
-    <script src="/assets2/vendor/datatables/dataTables.bootstrap4.min.js"></script>
+    <script src="/assets2/vendor2/datatables/jquery.dataTables.min.js"></script>
+    <script src="/assets2/vendor2/datatables/dataTables.bootstrap4.min.js"></script>
 
     <!-- Page level custom scripts -->
     <script src="/assets2/js/demo/datatables-demo.js"></script>
